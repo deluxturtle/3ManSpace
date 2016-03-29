a166ae2aade46ca5c60801d89e1c9492cb70cbdd
Andrew Seba <sebaturtle@gmail.com>
2016-03-29 14:47:53 -0500

    Added my own scene. and starting testing with input.

---
+++ FlightOfTheNephilim/Assets/_Scripts/Player/ShipTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("Right");
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            Debug.Log("Left");
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("Jump");
+        }
+	}
+}
