8b4e18ee1e883547c77dbe16a1046bddedcf05c6
Andrew Seba <sebaturtle@gmail.com>
2016-04-06 12:38:13 -0500

    Player input in!

---
+++ FlightOfTheNephilim/Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Handles movement for the player.
+/// </summary>
+public class Player : MonoBehaviour {
+
+    public float speed = 5f;
+    public float rotateSpeed = 0.5f;
+
+    public GameObject shipSprite;
+
+    private Vector3 velocity = Vector3.zero;
+    private float rotationX;
+    private float rotationY;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(horizontalInput < 0)
+        {
+
+        }
+        velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        if(velocity.magnitude != 0)
+        {
+            float angle = ((Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg) - 90);
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            shipSprite.transform.rotation = Quaternion.RotateTowards(shipSprite.transform.rotation, targetRotation, rotateSpeed);
+
+        }
+        transform.Translate(velocity * speed * Time.deltaTime);
+        Debug.DrawRay(transform.position, velocity);
+
+
+    }
+}
