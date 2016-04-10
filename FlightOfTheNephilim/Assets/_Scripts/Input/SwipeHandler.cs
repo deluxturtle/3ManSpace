f317f5e4d7f1ae0b280a9f2ce88c76865de4bb56
LivingValkyrie <deadwynn@gmail.com>
2016-04-09 19:44:53 -0500

    began work on Swipe controller
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ FlightOfTheNephilim/Assets/_Scripts/Input/SwipeHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Object = System.Object;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.net
+/// 
+/// Description: SwipeHandler 
+/// </summary>
+public class SwipeHandler : MonoBehaviour {
+
+	#region Fields
+
+	
+
+	#endregion
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
+
+public struct Swipe {
+
+	Vector3 startPos;
+	Vector3 endPos;
+
+	Vector2 velocity2D;
+	
+	//these are the objects that are raycast against for things like virtual joysticks
+	TouchZone startingZone;
+	TouchZone endingZone;
+
+	public enum Direction {
+		Left, Right, Up, Down
+	}
+} 
+
---
+++ FlightOfTheNephilim/Assets/_Scripts/Input/TouchZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.net
+/// 
+/// Description: TouchZone functions as single touch aswell as swipe zones until seperate functionality is needed.
+/// </summary>
+public class TouchZone : MonoBehaviour {
+
+	#region Fields
+
+	public int ID;
+
+	#endregion
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
