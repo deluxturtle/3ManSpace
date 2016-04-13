4dea671cb9a8db85c89cb9f18ec63d96a4f4f583
LivingValkyrie <deadwynn@gmail.com>
2016-04-13 14:37:22 -0500

    began testing of static classes
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ FlightOfTheNephilim/Assets/_Scripts/Static Class Testing/StaticCaller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.net
+/// 
+/// Description: StaticCaller 
+/// </summary>
+public class StaticCaller : MonoBehaviour {
+
+	#region Fields
+
+	#endregion
+
+	public void RunTest() {
+		StaticTest.Test();
+	}
+
+	public void RunTest2 (){
+		
+	}
+}
