39b371be126f6fc7c2f0ba31e8dfa555c666b328
Andrew Seba <sebaturtle@gmail.com>
2016-05-03 14:27:09 -0500

    Does it work

---
+++ FlightOfTheNephilim/Assets/_Scripts/PowerUps/PU_Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Health Power Up.
+/// </summary>
+public class PU_Health : MonoBehaviour {
+
+    public int healthBonus = 15;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
