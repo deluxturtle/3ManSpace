8a909500b35a5dea229e75e8ee421d158c817a67
Andrew Seba <sebaturtle@gmail.com>
2016-05-19 15:27:58 -0500

    Fixed main menu anchors

--- FlightOfTheNephilim/Assets/_Scripts/SoundManager.cs
+++ FlightOfTheNephilim/Assets/_Scripts/SoundManager.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class SoundManager : MonoBehaviour {
 
     public AudioClip playerShoot;
-
-
     public AudioClip powerUpPickup;
 
+    [HideInInspector]
+    public AudioSource audioSrc;
+
+    void Awake()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
 }
